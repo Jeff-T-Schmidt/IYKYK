104d2f33929c8da4858d57b0ac27e4c80c6251fa
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-06-03 13:54:37 -0700

    Header done

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-  
+
 // importing material UI components
 import AppBar from "@mui/material/AppBar";
 // import Box from "@mui/material/Box";
@@ -8,33 +8,16 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-  
+
+
 export default function Header() {
   return (
-      <AppBar position="static">
-        <Toolbar>
-          {/*Inside the IconButton, we 
-           can render various icons*/}
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/*This is a simple Menu 
-             Icon wrapped in Icon */}
-            <MenuIcon />
-          </IconButton>
-          {/* The Typography component applies 
-           default font weights and sizes */}
-  
-          <Typography variant="h6" 
-            component="div" sx={{ flexGrow: 1 }}>
-            GeeksforGeeks Header
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static" style={{ background: '#4285F4' }}>
+      <Typography variant="h3"
+        component="div" sx={{ flexGrow: 1 }} textAlign="center" color="black">
+        IYKYK
+      </Typography>
+
+    </AppBar>
   );
 }
