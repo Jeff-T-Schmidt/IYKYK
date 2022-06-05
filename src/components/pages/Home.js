2e05ee1503f2c2f715f2d02ad55d4405ce9b6c79
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-06-05 14:15:40 -0700

    Added some css to home / deleted and redid the footer, still not happy with it

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,42 @@
-const Home = () => <h1>Hello from the home page</h1>
+import Button from '@mui/material/Button';
+
+import './home.css'
+
+const Home = () => {
+    return (
+        <>
+            <div className='flex-container'>
+                <div className='headButtons'>
+                    <div className='row1'>
+                    <Button className="btn1"variant="contained">New Event</Button>
+                    </div>
+                    <div className='row2'>
+                    <Button className="btn2"variant="contained">My Invites</Button>
+                    </div>
+                    <div className='row3'>
+                    <Button className="btn3"variant="contained">Create Event</Button>
+                    </div>
+                </div>
+            </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
+                </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
+                </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
+                </div>
+        </>
+
+    )
+}
 
 export default Home
+
