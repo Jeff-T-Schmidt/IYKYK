dc07c44233babad7fd7e8f0d45f5654df8ed1f73
anoorishad <anoorishad@me.com>
2022-06-06 11:45:55 -0700

    adding API route functions

---
+++ src/utils/API.js
@@ -0,0 +1,58 @@
+//DEVELOP
+const BASE_URL="http://localhost:3001"
+//PROD
+// const BASE_URL="https://boardgames-back.herokuapp.com"
+
+module.exports = {
+    getAllUsers:()=>{
+        return fetch(`${BASE_URL}/api/users`).then(res=>res.json())
+    },
+    getOneUser:userId=>{
+        return fetch(`${BASE_URL}/api/users/${userId}`).then(res=>res.json())
+    },
+    verify:token=>{
+        return fetch(`${BASE_URL}/api/users/verifyToken`,{
+            headers:{
+                authorization:`Bearer ${token}`
+            }
+        }).then(res=>res.json())
+    },
+    login:userData=>{
+        return fetch(`${BASE_URL}/api/users/login`,{
+            method:"POST",
+            body:JSON.stringify(userData),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then(res=>res.json())
+    },
+    signup:userData=>{
+        return fetch(`${BASE_URL}/api/users`,{
+            method:"POST",
+            body:JSON.stringify(userData),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then(res=>res.json())
+    },
+    updateUser:updatedUserData=>{
+        return fetch(`${BASE_URL}/api/users/${id}`,{
+            method:"PUT",
+            body:JSON.stringify(updatedUserData),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then(res=>res.json())
+    },
+    
+    createEvent:(eventData,token)=>{
+        return fetch(`${BASE_URL}/api/events`,{
+            method:"POST",
+            body:JSON.stringify(eventData),
+            headers:{
+                "Content-Type":"application/json",
+                authorization:`Bearer ${token}`
+            }
+        }).then(res=>res.json())
+    }
+}
