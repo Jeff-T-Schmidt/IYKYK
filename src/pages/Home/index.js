06b50ac895e2a0fc2b6c87c03d2f5ebbb8d6a0f2
anoorishad <anoorishad@me.com>
2022-06-07 12:08:32 -0700

    protected routes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,49 +1,85 @@
 import Button from '@mui/material/Button';
-import {Link} from 'react-router-dom'
-
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import API from "../../utils/API"
 import './style.css'
 
-const Home = () => {
+const Home = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userId, setUserId] = useState(null);
+    const [token, setToken] = useState(null)
+    const navigate = useNavigate();
+    useEffect(() => {
+        const savedToken = localStorage.getItem("token");
+        if (savedToken) {
+            setToken(savedToken)
+        }
+    }, [])
+    useEffect(() => {
+        if (!props.isLoggedIn) {
+            // API.verify(token).then(userData => {
+            //     if (userData.userId) {
+            //         setIsLoggedIn(true);
+            //         setUserId(userData.userId)
+
+            //     } else {
+            //         setIsLoggedIn(false);
+            //         setUserId(null)
+            //         // navigate("/")
+            //     }
+            // })
+            navigate('/')
+        }
+        // else {
+        //     setIsLoggedIn(false);
+        //     setUserId(null)
+        //     // navigate("/")
+
+        // }
+    }
+    )
     return (
-        <>
-            <div className='flex-container'>
-                <div className='headButtons'>
-                    <div className='row1'>
-                        <Link to='/newEvent'>
-                    <Button className="btn1"variant="contained">My Events</Button>
-                        </Link>
+        <div>
+            <div>
+                <div className='flex-container'>
+                    <div className='headButtons'>
+                        <div className='row1'>
+                            <Link to='/newEvent'>
+                                <Button className="btn1" variant="contained">My Events</Button>
+                            </Link>
+                        </div>
+                        <div className='row2'>
+                            <Link to='/myinvites'>
+                                <Button className="btn2" variant="contained">My Invites</Button>
+                            </Link>
+                        </div>
+                        <div className='row3'>
+                            <Link to='/createevent'>
+                                <Button className="btn3" variant="contained">Create Event</Button>
+                            </Link>
+                        </div>
                     </div>
-                    <div className='row2'>
-                    <Link to='/myinvites'>
-                    <Button className="btn2"variant="contained">My Invites</Button>
-                    </Link>
-                    </div>
-                    <div className='row3'>
-                    <Link to='/createevent'>
-                    <Button className="btn3"variant="contained">Create Event</Button>
-                    </Link>
-                    </div>
+                </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
+                </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
+                </div>
+                <div className='eventBox1'>
+                    <div className='eventImage1'>Event Image</div>
+                    <div className='eventName1'>Event Name</div>
+                    <div className='eventDeets1'>Event Details</div>
                 </div>
             </div>
-                <div className='eventBox1'>
-                    <div className='eventImage1'>Event Image</div>
-                    <div className='eventName1'>Event Name</div>
-                    <div className='eventDeets1'>Event Details</div>
-                </div>
-                <div className='eventBox1'>
-                    <div className='eventImage1'>Event Image</div>
-                    <div className='eventName1'>Event Name</div>
-                    <div className='eventDeets1'>Event Details</div>
-                </div>
-                <div className='eventBox1'>
-                    <div className='eventImage1'>Event Image</div>
-                    <div className='eventName1'>Event Name</div>
-                    <div className='eventDeets1'>Event Details</div>
-                </div>
-        </>
-
+        </div>
     )
-}
 
+
+}
 export default Home
 
